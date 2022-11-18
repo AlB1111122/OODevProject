a6d0bec0b03898c3a0892b272c66b0ce4072e23a
A <101115956+AlB1111122@users.noreply.github.com>
2022-11-18 17:56:37 +0000

    cheff page progess edits to order, resturaunt, kitchen,customer, and cheff

---
+++ cs4013v1/src/main/java/com/example/oodevproj/CheffPage.java
@@ -0,0 +1,48 @@
+package com.example.oodevproj;
+
+import com.example.oodevproj.*;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class CheffPage extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    Restaurant restaurant;
+    Kitchen kitchen;
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Cheff page");
+        restaurant = new Restaurant(1);
+        Dish d1 = new Dish(3);
+        Customer c = new Customer("ban",78296653);
+        Order o = new Order("2",c);
+        o.addDish(d1);
+        kitchen.addOrder(o);
+
+        ListView<String> waitingList = new ListView<>();
+        ObservableList<String> waitDish = FXCollections.observableArrayList (kitchen.getWatingDishString());
+        waitingList.setItems(waitDish);
+        waitingList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
+        Pane waitingListPane = new Pane(waitingList);
+        waitingListPane.setPrefSize(400,350);
+        Scene pageBase = new Scene(waitingListPane,1000,500);
+        stage.setScene(pageBase);
+        stage.show();
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+        kitchen = restaurant.getKitchen();
+    }
+}
