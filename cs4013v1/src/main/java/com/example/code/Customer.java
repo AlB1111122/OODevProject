ee7a5f917003baa3941fd3be991d88eadb771a01
ciancarmody <101117001+ciancarmody@users.noreply.github.com>
2022-11-23 12:28:07 +0000

    updated javadoc

--- cs4013v1/src/main/java/com/example/code/Customer.java
+++ cs4013v1/src/main/java/com/example/code/Customer.java
@@ -1,6 +1,17 @@
 package com.example.code;
 
 public class Customer extends Person{
+
+//*
+    // This extends the Person class.
+    // @param name          returns name of customer   (extended from Person)
+    // @param phoneNumber   returns number of customer (extended from Person)
+    // @param ID            returns ID of customer     (extended from Person)
+    //
+    // @see Person class
+    // */
+    
+    
     private static int noCustomers = 0;
     Customer(String name, int phoneNumber){//public for testing reset later
         super(name, phoneNumber,"C" + noCustomers);
