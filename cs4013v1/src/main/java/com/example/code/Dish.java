19be3ca8159de8601eed26e1dfb988cd03066d03
ciancarmody <101117001+ciancarmody@users.noreply.github.com>
2022-11-23 12:32:30 +0000

    added javadoc

--- cs4013v1/src/main/java/com/example/code/Dish.java
+++ cs4013v1/src/main/java/com/example/code/Dish.java
@@ -1,6 +1,18 @@
 package com.example.code;
 
 public class Dish{
+
+ /*
+    * This is the Dish class
+    * @param orderId        - ID to represent the dish in the menu
+    * @param dishes         - list of Dishes available to eat
+    * @param dishesPrice    - list of prices for respective dishes
+    * @param dishName       - name of specific dish
+    * @param price          -  price of specific dish
+    * @param ready          - boolean of dish being ready (true/false)
+    * @param notes          - notes for input (allergies, etc.)
+    * */
+
     private int orderId;
     private String[] dishes = {"BBQ wings","Spring rolls","Nachos","Bruschetta",
             "Peperoni pizza","Margherita pizza","Cheese burger with lettuce and tomato","Vegetable soup","Carbonara","Steak served with roast vegtables",
