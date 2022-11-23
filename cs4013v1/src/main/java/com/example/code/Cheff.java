9bcf286a32c6e928ad8a6071150ea1eb0d24e046
ciancarmody <101117001+ciancarmody@users.noreply.github.com>
2022-11-23 12:25:03 +0000

    updated javadoc

--- cs4013v1/src/main/java/com/example/code/Cheff.java
+++ cs4013v1/src/main/java/com/example/code/Cheff.java
@@ -3,12 +3,15 @@ package com.example.code;
 public class Cheff extends Employee{
     private static int noCheffs = 0;
 
-    /**
-     * constructs a new cheff
-     * @param name the name of the cheff
-     * @param phoneNumber the cheffs phone number
-     * @param password the cheffs password(needed to sign in)
-     */
+    //*
+    // This extends the Employee class and constructs a Cheff.
+    // @param name          (extended from Employee > Person)
+    // @param phoneNumber   (extended from Employee > Person)
+    // @param password      (extended from Employee > Person)
+    // @param ID            (extended from Employee > Person)
+    //
+    // @see Employee class
+    // */
     Cheff(String name, int phoneNumber,String password){
         super(name, phoneNumber, "Ch" + noCheffs,password);
         noCheffs++;
